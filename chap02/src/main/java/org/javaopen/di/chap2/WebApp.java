d59e25c8acce4302a1387b5f5f76d7af73cdf2af
Yasuyuki ENDO <eyasuyuki@gmail.com>
2025-03-19 05:23:34 +0900

    move main to WebApp

--- chap02/src/main/java/org/javaopen/di/chap2/WebApp.java
+++ chap02/src/main/java/org/javaopen/di/chap2/WebApp.java
@@ -1,6 +1,5 @@
 package org.javaopen.di.chap2;
 
-import org.apache.commons.fileupload2.core.RequestContext;
 import org.apache.wicket.Component;
 import org.apache.wicket.Page;
 import org.apache.wicket.Session;
@@ -8,18 +7,63 @@ import org.apache.wicket.authorization.Action;
 import org.apache.wicket.authorization.IAuthorizationStrategy;
 import org.apache.wicket.authroles.authentication.AbstractAuthenticatedWebSession;
 import org.apache.wicket.authroles.authentication.AuthenticatedWebApplication;
-import org.apache.wicket.authroles.authorization.strategies.role.metadata.MetaDataRoleAuthorizationStrategy;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.protocol.http.WebSession;
+import org.apache.wicket.protocol.ws.javax.WicketServerEndpointConfig;
 import org.apache.wicket.request.component.IRequestableComponent;
 import org.apache.wicket.request.cycle.RequestCycle;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.request.resource.IResource;
+import org.eclipse.jetty.jmx.MBeanContainer;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.websocket.jakarta.server.config.JakartaWebSocketServletContainerInitializer;
 import org.javaopen.di.chap2.ui.AuthSession;
 import org.javaopen.di.chap2.ui.HomePage;
 import org.javaopen.di.chap2.ui.LoginPage;
 
+import javax.management.MBeanServer;
+import java.lang.management.ManagementFactory;
+
 public class WebApp extends AuthenticatedWebApplication {
+
+    private static Server server;
+
+    public static void initServer() throws Exception {
+        System.setProperty("wicket.configuration", "development");
+
+        server = new Server();
+        HttpConfiguration httpConfig = new HttpConfiguration();
+        httpConfig.setOutputBufferSize(32768);
+
+        ServerConnector http = new ServerConnector(server, new HttpConnectionFactory(httpConfig));
+        http.setPort(8080);
+        http.setIdleTimeout(1000 * 60 * 60);
+        server.addConnector(http);
+
+        WebAppContext webAppContext = new WebAppContext();
+        webAppContext.setServer(server);
+        webAppContext.setContextPath("/");
+        webAppContext.setWar("src/main/webapp");
+
+        ServletContextHandler contextHandler =
+                ServletContextHandler.getServletContextHandler(webAppContext.getServletContext());
+        JakartaWebSocketServletContainerInitializer.configure(
+                contextHandler,
+                (servletContext, container) -> container.addEndpoint(new WicketServerEndpointConfig())
+        );
+
+        server.setHandler(webAppContext);
+
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        MBeanContainer mBeanContainer = new MBeanContainer(mBeanServer);
+        server.addEventListener(mBeanContainer);
+        server.addBean(mBeanContainer);
+    }
+
     @Override
     protected void init() {
         super.init();
@@ -48,6 +92,17 @@ public class WebApp extends AuthenticatedWebApplication {
             }
         });
         mountPackage("/login", LoginPage.class);
+    }
+
+    public static void main(String[] args) {
+        try {
+            initServer();
+            server.start();
+            server.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(100);
+        }
     }
 
     @Override
